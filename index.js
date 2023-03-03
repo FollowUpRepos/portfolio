a7481d4cf0f7225b6f8cbcb60664d26c1088766b
James Newton <blackslate@lexogram.com>
2023-03-03 16:05:02 +0300

    Fix selection of WebDev|Creativity|(Physics)
    * Create generic showOnly() function
    * For each section, create xxxHeading and allXxx variables
    * Store all xxxHeading variables in an allHeadings array
    * Store all allXxxx variables in an allInfo array
    * Add a custom listener to each Heading element
    * In the custom listener, call the showOnly() function, with the heading and the info variables for the elements that need to be shown
    * In showOnly():
      - Iterate through allHeadings, switch on the chosen heading and switch off all others
      - Iterate through allInfo, switch on the chosen info elemnts and switch off all others

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// const webHeading = document.getElementsByClassName('showStyle')[0];
-// const allSectionWeb =document.getElementsByClassName("hi")
-
+const webHeading = document.getElementsByClassName('showStyle')[0];
+const allSectionWeb = Array.from(document.getElementsByClassName("hi"))
 // function testClick(){
 //   webHeading.classList.toggle("headline--active")
 
-// Array.from(allSectionWeb).map(ele=>{
+// allSectionWeb.map(ele=>{
 //     ele.classList.toggle("d-none")  
 //     ele.classList.toggle("card-hidden")
 //     ele.classList.toggle("fullWidth")
@@ -15,23 +14,73 @@
 // }
 
 
-// webHeading.addEventListener("click",testClick);
-
+// Show only Creativity elements when h3.creativStyle is clicked 
 const createHeading = document.getElementsByClassName('creativStyle')[0];
-const allCreativ = document.getElementsByClassName('creativ');
+const allCreativ = Array.from(document.getElementsByClassName('creativ'));
 console.log(allCreativ)
-function create() {
-    console.log("hello")
-    createHeading.classList.toggle("headline--active")
-    Array.from(allCreativ).map(ele=>{
+
+// function create() {
+//     console.log("hello")
+//     createHeading.classList.toggle("headline--active")
+//     allCreativ.map(ele=>{
         
-            ele.classList.toggle("d-none")  
-            ele.classList.toggle("card-hidden")
-            ele.classList.toggle("fullWidth")
-            testClick()
-            return
+//             ele.classList.toggle("d-none")  
+//             ele.classList.toggle("card-hidden")
+//             ele.classList.toggle("fullWidth")
+//             testClick()
+//             return
      
-     })
+//      })
+// }
+
+// createHeading.addEventListener("click", create)
+
+
+const allHeadings = [ webHeading, createHeading ]
+const allInfo = [ allSectionWeb, allCreativ ]
+
+
+const showOnly = ( header, info ) => {
+    // HEADINGS
+    allHeadings.forEach( heading => {
+        if (heading === header) {
+            // Highlight this heading
+            heading.classList.add("headline--active")
+        } else {
+            // Dim this heading
+            heading.classList.remove("headline--active")
+        }
+    })
+
+    // INFO
+
+    allInfo.forEach( anInfo => {
+        if (anInfo === info) {
+            anInfo.map( ele => {
+                ele.classList.remove("d-none")  
+                ele.classList.remove("card-hidden")
+                ele.classList.add("fullWidth")
+            })
+        } else {
+            anInfo.map( ele => {
+                ele.classList.add("d-none")  
+                ele.classList.add("card-hidden")
+                ele.classList.remove("fullWidth")
+            })
+        }
+    })
 }
 
-createHeading.addEventListener("click", create)
+
+const webClick = () => {
+  showOnly( webHeading, allSectionWeb )
+}
+
+
+const createClick = () => {
+  showOnly( createHeading, allCreativ )
+}
+
+
+webHeading.addEventListener("click", webClick);
+createHeading.addEventListener("click", createClick)
